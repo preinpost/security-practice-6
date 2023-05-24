e905dab65cd6c9e49baed45c3fb7a0ad1145342d
minsoo.lee <minsoo.lee@8byte.co.kr>
2023-05-24 14:17:28 +0900

    project init

---
+++ src/main/java/hello/demo/SecurityChapter6Application.java
@@ -0,0 +1,13 @@
+package hello.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityChapter6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityChapter6Application.class, args);
+    }
+
+}
---
+++ src/test/java/hello/demo/SecurityChapter6ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityChapter6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
